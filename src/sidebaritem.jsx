9c9909f8a3536a0e4d6bea5346e602455cc397a3
pooh <cktjdgus45@naver.com>
2022-02-15 20:19:01 +0900

    clean photoshop

--- src/sidebaritem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SidebarItem = ({ name, active, handleClick }) => {
-    return (
-        <button onClick={handleClick} className={`sidebar-item ${active ? 'active' : ''}`}>{name}</button>
-    )
-}
-
-export default SidebarItem;
