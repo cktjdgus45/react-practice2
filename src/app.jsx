ad3f9a3d17127f0f9f37f605d4162f9da9045f16
pooh <cktjdgus45@naver.com>
2022-02-17 09:25:17 +0900

    get pocketmons form pocke api

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './app.css';
 import Pagination from './pagination';
 import PocketmonList from './pocketmonList';
 
 function App() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon")
+    .then(res => setPokemon(res.data.results.map(p => p.name)))
+
   return (
     <>
       {
