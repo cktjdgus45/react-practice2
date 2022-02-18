34913d43cbfa89ce72bb2212b29c55e2a0f5caa5
pooh <cktjdgus45@naver.com>
2022-02-18 11:09:23 +0900

    clean-up useEffect

--- src/app.jsx
+++ src/app.jsx
@@ -6,10 +6,28 @@ import PocketmonList from './pocketmonList';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
-  axios
-    .get("https://pokeapi.co/api/v2/pokemon")
-    .then(res => setPokemon(res.data.results.map(p => p.name)))
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false);
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+    return () => cancel();  //clean-up
+  }, [currentPageUrl])
+
+
+  if (loading) return "loading...";
+
 
   return (
     <>
