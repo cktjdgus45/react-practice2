6337b9c7df6ae301ef96773641010a7e0b5abbe6
pooh <cktjdgus45@naver.com>
2022-02-18 12:38:55 +0900

    clean pocketmon app

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,10 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './app.css';
-import Pagination from './pagination';
-import PocketmonList from './pocketmonList';
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
-  const [nextPageUrl, setNextPageUrl] = useState();
-  const [prevPageUrl, setPrevPageUrl] = useState();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    let cancel;
-    axios.get(currentPageUrl, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(res => {
-      setLoading(false);
-      setNextPageUrl(res.data.next)
-      setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map(p => p.name))
-    })
-    return () => cancel();  //clean-up
-  }, [currentPageUrl])
-
-
-  function gotoNextPage() {
-    setCurrentPageUrl(nextPageUrl);
-  }
-  function gotoPrevPage() {
-    setCurrentPageUrl(prevPageUrl);
-  }
-
-  if (loading) return "loading...";
-
-
   return (
     <>
-      <PocketmonList pokemon={pokemon} />
-      <Pagination
-        gotoNextPage={nextPageUrl ? gotoNextPage : null}
-        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
-      />
+      clean
     </>
   );
 }
