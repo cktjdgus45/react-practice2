7233103531653434acf9ab3d94bc780ee23b7ad4
pooh <cktjdgus45@naver.com>
2022-02-24 10:42:24 +0900

    get questions by count,category from api

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import FlashCardList from './flashcardList';
 import axios from 'axios';
 import './app.css';
 
 function App() {
   const [flashcards, setFlashcards] = useState([]);
-  console.log(flashcards)
+  const [categories, setCategories] = useState([]);
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
   useEffect(() => {
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories);
+      })
+  }, [])
+
+
+  useEffect(() => {
+
+  }, [])
+
+  function decodingString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get('https://opentdb.com/api.php?', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
       .then(res => {
         setFlashcards(
           res.data.results.map((questionItem, index) => {
@@ -23,16 +51,33 @@ function App() {
           })
         )
       });
-  }, [])
-
-  function decodingString(str) {
-    const textArea = document.createElement('textarea');
-    textArea.innerHTML = str;
-    return textArea.value;
   }
 
   return (
     <>
+      <form className='header' onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {
+              categories.map(category => {
+                return (
+                  <option value={category.id} key={category.id}>
+                    {category.name}
+                  </option>
+                )
+              })
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amout">Numbers of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
       <div className="container">
         <FlashCardList flashcards={flashcards} />
       </div>
--- src/flashcard.jsx
+++ src/flashcard.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const FlashCard = ({ flashcard }) => {
     const [flip, setFlip] = useState(false);
+
     return (
         <div className={`card ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
             <div className="front">
                 {flashcard.questions}
                 <div className="flashcard-options">
                     {flashcard.options.map(option => {
-                        return <div className='flashcard-option'>{option}</div>
+                        return <div className='flashcard-option' key={option}>{option}</div>
                     })}
                 </div>
             </div>
