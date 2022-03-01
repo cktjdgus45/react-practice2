a159776ef47e114bc7945930ee3372bec0b1b924
pooh <cktjdgus45@naver.com>
2022-03-01 19:56:34 +0900

    intialize merge conflict on pull working

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -1,5 +1,5 @@
+import { Container } from 'react-bootstrap';
 import '../app.css';
-<<<<<<< HEAD
 import { AuthProvider } from '../contexts/authContext';
 import Signup from './signup';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -7,13 +7,10 @@ import Dashboard from './dashboard';
 import Login from './login';
 import PrivateRoute from './privateRoute';
 import ForgotPassword from './forgotPassword';
-=======
->>>>>>> 28a4b010c8fa4935d0f4aa63822fdce8d4699cdb
 
 function App() {
 
   return (
-<<<<<<< HEAD
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className='w-100' style={{ maxWidth: '400px' }}>
         <Router>
@@ -30,11 +27,6 @@ function App() {
         </Router>
       </div>
     </Container>
-=======
-    <>
-      clean
-    </>
->>>>>>> 28a4b010c8fa4935d0f4aa63822fdce8d4699cdb
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/app.jsx';
-
+import './service/firebase.js';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
