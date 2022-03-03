76fd2a9db90b548a8ebb3e15cad777bf272a72a1
pooh <cktjdgus45@naver.com>
2022-03-03 23:00:14 +0900

    transfer habits-count to navbar Component

--- src/app.jsx
+++ src/app.jsx
@@ -5,10 +5,17 @@ import Navbar from './components/navbar';
 import HabitInput from './components/habitInput';
 
 class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Codinng', count: 0 },
+    ]
+  }
   render() {
     return (
       <>
-        <Navbar />
+        <Navbar habitsCount={this.state.habits.length} />
         <HabitInput />
         <Habits />
       </>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,12 @@ import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends Component {
     render() {
+        const { habitsCount } = this.props;
         return (
             <nav className='nav'>
                 <FontAwesomeIcon icon={faLeaf} className='logo' />
                 <h3 className="title">Habit Tracker</h3>
-                <div className="habits-count">0</div>
+                <div className="habits-count">{habitsCount}</div>
             </nav>
         );
     }
